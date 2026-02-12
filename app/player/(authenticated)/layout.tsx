e08c999e9358d033303cd1199c2446f50007cded
mathisenault-pixel <mathisenault@gmail.com>
2026-02-12 05:26:03 +0100

    update: relance du projet

--- app/player/(authenticated)/layout.tsx
+++ app/player/(authenticated)/layout.tsx
@@ -66,12 +66,8 @@ export default function PlayerAuthLayout({
                   <path strokeLinecap="round" strokeLinejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
                 </svg>
               </Link>
-              <span className="inline-flex lg:rounded-full lg:border lg:border-black/20 lg:p-0.5 lg:-ml-1">
-                <LanguageSwitcher />
-              </span>
-              <span className="inline-flex lg:rounded-full lg:border lg:border-black/20 lg:p-0.5">
-                <HeaderUserMenu />
-              </span>
+              <LanguageSwitcher />
+              <HeaderUserMenu />
             </div>
           </div>
           <div className="lg:hidden px-2.5 py-2.5 overflow-x-auto">
